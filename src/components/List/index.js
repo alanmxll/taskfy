6191091dcedb0ce40bb51d32dfebb7bcfc15b52b
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-01-07 20:58:14 -0300

    Created: List component

---
+++ src/components/List/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { MdAdd } from "react-icons/md";
+
+import Card from "../Cards";
+
+import { Container } from "./styles";
+
+function List() {
+  return (
+    <Container>
+      <header>
+        <h2>Tasks</h2>
+        <button type="button">
+          <MdAdd size={24} color="FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ul>
+    </Container>
+  );
+}
+
+export default List;
