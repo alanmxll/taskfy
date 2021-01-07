0d194601e12ed065d52fd1fa4b1f9d6a8facb65a
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-01-07 20:58:34 -0300

    Created: Card component

---
+++ src/components/Cards/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Container, Label } from "./styles";
+
+function Cards() {
+  return (
+    <Container>
+      <header>
+        <Label color="#3dc050" />
+      </header>
+      <p>Learn about Drag n Drop function</p>
+      <img
+        src="https://api.adorable.io/avatars/285/10@adorable.io.png"
+        alt=""
+      />
+    </Container>
+  );
+}
+
+export default Cards;
