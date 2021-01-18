407cb7b9a8e6b6ff58316829f6bc8239e5aee853
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-01-17 22:34:43 -0300

    Removed: makeStyles of library, changed do styled components

--- src/components/Modals/createTask.js
+++ src/components/Modals/createTask.js
@@ -1,33 +1,23 @@
 import React from "react";
 
-import { useStyles } from "./modalStyles";
 import { Container, HeaderModal } from "./styles";
 
 import { RiCloseFill } from "react-icons/ri";
 import { FaRegEdit } from "react-icons/fa";
 
 function CreateTask({ onClose }) {
-  const classes = useStyles();
-
   return (
     <Container>
-      <div className={classes.closeButton} title="Close" onClick={onClose}>
-        <RiCloseFill size={24} color="0f7b1e" />
+      <div className="closeButton" title="Close" onClick={onClose}>
+        <RiCloseFill size={30} color="0f7b1e" />
       </div>
-      <div className={classes.contentCard}>
+      <div className="contentCard">
         <div className="colorTop" />
         <HeaderModal>
           <FaRegEdit size={40} />
-          <div style={{ paddingLeft: "5px" }}>
-            <h2
-              id="simple-modal-title"
-              style={{ color: "#888", fontSize: "16px" }}
-            >
-              Task Manager
-            </h2>
-            <h1 id="simple-modal-title" style={{ fontSize: "20px" }}>
-              Form
-            </h1>
+          <div className="simple-modal-title">
+            <h2 className="simple-modal-title">Task Manager</h2>
+            <h1 className="simple-modal-title">Form</h1>
           </div>
         </HeaderModal>
       </div>
--- src/components/Modals/styles.js
+++ src/components/Modals/styles.js
@@ -8,6 +8,29 @@ export const Container = styled.div`
     border-top-left-radius: 5px;
     border-top-right-radius: 5px;
   }
+
+  div.closeButton {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    position: absolute;
+    top: 1%;
+    left: 96.6%;
+    width: 35px;
+    height: 35px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  div.contentCard {
+    position: absolute;
+    top: 5.5%;
+    left: 72.5%;
+    width: 500px;
+    background-color: #fff;
+    border-radius: 5px;
+  }
 `;
 
 export const HeaderModal = styled.div`
@@ -15,4 +38,17 @@ export const HeaderModal = styled.div`
   color: #0f7b1e;
   padding: 30px;
   padding-left: 35px;
+
+  div.simple-modal-title {
+    padding-left: 5px;
+  }
+
+  h2.simple-modal-title {
+    color: #888;
+    font-size: 16px;
+  }
+
+  h1.simple-modal-title {
+    font-size: 20px;
+  }
 `;
